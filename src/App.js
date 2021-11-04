221a061c4abf3df066c94b0bef581c6ea35ed4f4
steffan <steffancmorris@gmail.com>
2021-11-04 16:42:22 -0700

    skeletonv1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+
+import { H2, H5, Text } from "@blueprintjs/core";
+
+import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <H2>Functional Justice</H2>
+      <br />
+      <H5>How It Works</H5>
+      <Text>
+        1. Create an account. <br /> 2. Take steps to improve your life with the
+        help of our participating providers, who will add a record to your
+        account.
+        <br /> 3. Then use the records of all your hard work to ask prosecutors
+        and courts to reduce your charges or sentences.
+        <br />
+      </Text>
+      <br />
+      <H5>Ready to get started?</H5>
+      <Text>
+        For service providers, public defenders, prosecutors, and courts
+        interested in participating, please email us{" "}
+        <a href="mailto: functional@streetdemocracy.org">
+          functional@streetdemocracy.org
         </a>
-      </header>
+        .
+      </Text>
+      <Button className="button" variant="contained">
+        Sign up
+      </Button>
     </div>
   );
 }
